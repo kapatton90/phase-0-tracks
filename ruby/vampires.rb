435f400d80ef1cb97e681c8b3247897583b611b5
kapatton19 <kapatton19@gmail.com>
2016-05-25 21:35:10 -0500

    Finished Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts" How old are you?
+age = gets.chomp
+
+puts "What year were you born?"
+year_born = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order you some?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
