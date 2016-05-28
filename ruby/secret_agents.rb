8e7946964b3340f58668ac7f736379981080c7e4
kapatton19 <kapatton19@gmail.com>
2016-05-28 09:04:57 -0500

    Link stylesheet to html file

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,14 +1,44 @@
-def encrypt (str)
+def encrypt (message)
   index = 0 
   
   
-while index < str.length
-   #do something here
-   str[index] = str[index].next!
-   index += 1
-    
-  end
-  str
+	while index < message.length 
+	#figure out the length of the message
+  	message[index] = message[index].next! 
+  	#figure out the index of the message and then change it to the next index placement
+   	index += 1
+   	#counter to ensure no infinite loops   
+  	end
+  	message
 end
 
 puts encrypt("secret message")
+
+
+
+def decrypt(message)
+
+placement = "abcdefghijklmnopqrstuvwxyz".index("message")
+
+placement = placement - 1
+puts placement
+
+end
+
+
+
+def decrypt(message) 
+	index = 0
+
+	while index < message.length
+		p message[index]
+		index += 1
+	end
+  	
+	def decrypt_2(message)
+		placement = "abcdefghijklmnopqrstuvwxyz".index("message")
+		placement + 1
+	
+	
+	end	
+end
