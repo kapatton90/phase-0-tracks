0a9d1a8dce8eed8a0c3419fce5445bcd51c04f90
kapatton19 <kapatton19@gmail.com>
2016-05-30 14:11:11 -0500

    Fix real age variable

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,7 +4,7 @@ employees = gets.chomp.to_i
 #Run program until all employees have been processed.
 counter = -1
 until counter >= employees
-	puts 
+	
 	counter += 1
 end	
 
@@ -17,15 +17,15 @@ else
 	print "Results inconclusive."
 end		
 
-print "How old are you?"
+puts "How old are you?"
 age = gets.chomp.to_i
 
-print "What year were you born?"
+puts "What year were you born?"
 year_born = gets.chomp.to_i
 
 current_year = 2016
 
-def real_age
+def real_age(year_born, current_year, age)
 
 #Check to see if their age and year born make sense.
 	if year_born == current_year - age || year_born == current_year - (age +1)
@@ -36,7 +36,7 @@ def real_age
 	return real_age	
 end
 
-print "Our company cafeteria serves garlic bread. Should we order you some?(Yes/No)"
+puts "Our company cafeteria serves garlic bread. Should we order you some?(Yes/No)"
 garlic_bread = gets.chomp
 
 if garlic_bread == "Yes"
@@ -54,28 +54,35 @@ else
 	false
 end	
 
-puts "Do you have any allergies?"
+puts "Do you have any allergies(type done when finished with this section)?"
 #Loop for multiple allergies
-while allergies = gets.chomp
+while 1==1
+	allergies = gets.chomp
 	case allergies
-	when allergies = ""
-	puts "Do you have any allergies?"
-	break
-	else allergies == "done"
+	when "sunshine"
+		puts "Proably a Vampire."
+		break
+	when "done"
+		break
+	else 
+		puts "Do you have any other allegeries?"
 	end
 end
 
 
-wolves_like_sunshine == true
-wolves_like_garlic == true
-vampires_like_sunshine == false
-vampires_like_garlic == false
+	
 
-if real_age == true && garlic_bread == true
+
+$wolves_like_sunshine == true
+$wolves_like_garlic == true
+$vampires_like_sunshine == false
+$vampires_like_garlic == false
+
+if real_age(year_born, current_year, age) == true && garlic_bread == true
 	print "Probably not a vampire."
-elsif real_age == false && garlic_bread == false	
+elsif real_age(year_born, current_year, age) == false && garlic_bread == false	
 	print"Probably a vampire."
-elsif real_age == false && garlic_bread == false && insurance == false
+elsif real_age(year_born, current_year, age) == false && garlic_bread == false && insurance == false
 	print "Almost certainly a vampire."
 elsif name == "Drake Cula" || name == "Tu Fang"
 	print "Definitely a vampire."		
@@ -84,4 +91,4 @@ else
 end	
 
 #Right before program exits
-#print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+print "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
