d4403387d7a9e885aca9bfee283938b7a199964d
kapatton19 <kapatton19@gmail.com>
2016-06-04 15:00:22 -0500

    Insert comments on exercise

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,34 +1,33 @@
 # Jessica Bohn & Kimberly Patton
 # 06/03/2016
-
+# method to insert new items into an array
 def method_of_arrays(array, y)
 	new_array = array << y
 end
-
+# method to build an array
 def build_array(x, y, z)
 	array = [x, y, z]
 end	
-
+# create array 
 books = []
 p books
-
+# add five items to the array
 books = ["Harry Potter", "The Great Gatsby", "The Lord of the Rings", "The Hunger Games", "Pride & Prejudice"]
 p books
-
+# delete the item at index 2
 books.delete_at(2)
 p books
-
+# insert a new item at index 2
 books.insert(2, "The Adventures of Huckleberry Finn")
 p books
-
+# remove the first item of the array without using it's index
 books.shift 
 p books
-
+# ask the array whether it has a certain item. 
 p "The book The Great Gatsby appears in this array: #{books.include?("The Great Gatsby")}"
-# p books.bsearch {|x| x == "The Hunger Games"}
-
+# create another array
 colors = ["blue", "pink", "green"]
-
+# add book and colors arrays together in new books_and_colors array
 books_and_colors = books + colors
 p books_and_colors
 
