7c429bea3a7f4c908bf5568bffc50ef3e2674846
kapatton19 <kapatton19@gmail.com>
2016-06-06 11:49:39 -0500

    Time is up for assessment, working on numbers 4 and 5

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -11,14 +11,18 @@ p calculate(6, '/', 3)
 
 #ask user for input
 puts "What calculation would you like to perform?(Type done when finished)"
-	calculation = gets.chomp.split
-	#convert user input to the correct format for calculate method
-	p calculate(calculation[0].to_i, calculation[1], calculation[2].to_i )
+calculation = gets.chomp.split
+#convert user input to the correct format for calculate method
+p calculate(calculation[0].to_i, calculation[1], calculation[2].to_i )
+
 #until loop for as many calculations as user wants, until done is typed
-until  calculation == "done"
-	#ask user for input
+begin 	
 	puts "What calculation would you like to perform?(Type done when finished)"
 	calculation = gets.chomp.split
 	#convert user input to the correct format for calculate method
 	p calculate(calculation[0].to_i, calculation[1], calculation[2].to_i )
-end		
+end	until calculation == "done"	
+#store calculator history
+calculator_history = {
+	calculation => calculate
+}
