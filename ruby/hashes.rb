bc4d48241e240c394d6ac071c40a9c9c76831c84
kapatton19 <kapatton19@gmail.com>
2016-06-06 09:24:42 -0500

    Change question format, add more explanation

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,20 +6,25 @@ interior_designer = {
 	:decor_theme => " " 
 
 }
-puts "What is the client's name?"
+#section for name
+puts "Name:"
 name = gets.chomp
+#convert to symbol and store in hash
 interior_designer[:name] = name.to_sym
 
-puts "what is the client's age?"
+puts "Age:"
 age = gets.chomp
+#convert to symbol and store in hash
 interior_designer[:age] = age.to_sym
 
-puts "How many children do they have?"
+puts "How many children do you have?"
 children = gets.chomp.to_sym
+#convert to symbol and store in hash
 interior_designer[:children] = children.to_sym
 
 puts "What is the theme of the decor?"
 decor = gets.chomp.to_sym
+#convert to symbol and store in hash
 interior_designer[:decor_theme] = decor.to_sym
 
 p interior_designer
@@ -27,11 +32,13 @@ p interior_designer
 puts "Do you need to make any changes to your answers?(yes/none)"
 changes = gets.chomp
 
+#create if statement to input changes if user need to
 if changes == "yes"
-	puts "What section do you need to make changes to? name; age; children; or decor?"
+	puts "What section would you like to make changes to? name; age; children; or decor?"
 	section = gets.chomp.to_sym
 	puts "What would you like to change it to?"
 	answer_change = gets.chomp
+	#convert to symbol and store in hash
 	interior_designer[section] = answer_change.to_sym	
 end	
 
