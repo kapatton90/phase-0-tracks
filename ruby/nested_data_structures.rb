c2e9da5054ec500bb8f3e53676713321e98ca03a
kapatton19 <kapatton19@gmail.com>
2016-06-05 20:23:20 -0500

    Create an array within a hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,15 @@
+grocery_store = {
+	"produce" => { 
+		"fruits" => ["apples", "oranges", "watermelon"],
+		"vegetables" => ["lettuce", "cucumber", "onion"]},
+	"meat" => {
+		"pork" => ["bacon", "sausage", "ham"],
+		"chicken" => ["breast", "thighs"],
+		"red meat" =>["steak", "ground hamburger", "beef tips"]},
+	"bakery" => {
+		"bread" => ["baguette", "roll", "loaf"],
+		"sweets" => ["cake", "cupcakes", "cookies"]}
+}
+
+#find cake
+grocery_store["bakery"][1[2]]]
