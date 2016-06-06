2c002b3d51d46dba6302e2b48f70103af4526d50
kapatton19 <kapatton19@gmail.com>
2016-06-05 22:33:07 -0500

    Split out the letters of the agent's name. Also create an array for vowels and consonants.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,6 +3,20 @@ puts "Name please:"
 agent_name = gets.chomp
 #swap the first and last names
 p agent_name.split(" ").reverse!
+#split the agent's name into letters 
+p letters = agent_name.chars
+#array for vowels
+p vowels = ["a", "e", "i", "o", "u"]
+#array for consonants
+p consonants = "bcdfghjklmnpqrstvwxyz".chars
+
+# next_vowel = vowels.split
+# 	vowels.next
+	
+
+# if agent_name.include?(vowels)
+# 	put next_vowel
+# end
 
 
 
