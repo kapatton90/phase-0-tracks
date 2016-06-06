384ff60c0324586a10f65966fa184c6401142388
kapatton19 <kapatton19@gmail.com>
2016-06-06 10:02:43 -0500

    Begin practice assessment for Section 5

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,19 +1,39 @@
+def reverse_agent_name 
+
+#swap the first and last names
+
+	agent_name.split(" ").reverse!
+end	
 #ask for the agent's name
 puts "Name please:"
 agent_name = gets.chomp
-#swap the first and last names
-agent_name.split(" ").reverse!
+
+p reverse_agent_name
+
 #split the agent's name into letters 
-letters = agent_name.chars
+letters = reverse_agent_name{|name| name != name.chars}
+p letters
+#method for finding the next letter 
+def next_letter
 #array for vowels
 vowels = "aeiou".chars
 #array for consonants
-consonants = "bcdfghjklmnpqrstvwxyz".chars
-#method for next vowel
-def next_vowel 
-	if letters == vowels 
-	puts vowels.next
-end	
+	if [letters] == vowels
+		puts vowels.next
+	else
+		puts letters.next
+	end
+end
+
+p next_letter
+
+# consonants = "bcdfghjklmnpqrstvwxyz".chars
+# #method for next vowel
+# def next_vowel 
+# 	if letters == vowels 
+# 	puts vowels.next
+# end	
+# agent_name[/(?<vowel>[aeiou](?<non-vowel>[^aeiou])/, "vowel"]
 
 #check letters of agent's name for vowels and consonants
 # if letters == vowels
@@ -23,13 +43,13 @@ end
 # 	|letter| letter.next
 # 	}
 # end		
-if letters.include? vowels
-	puts next_vowel
-elsif letters.include?(consonants)
-	puts next_consonant
-else
-	puts "error"
-end
+# if letters.include? vowels
+# 	puts next_vowel
+# elsif letters.include?(consonants)
+# 	puts next_consonant
+# else
+# 	puts "error"
+# end
 
 
 			
