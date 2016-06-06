6dfd6c0f858a664515e1b9fca56e1f562a99aa46
kapatton19 <kapatton19@gmail.com>
2016-06-06 18:55:35 -0500

    Stop exercise in order to complete pairing exercise

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,62 +3,30 @@ puts "Name please:"
 def agent_name
  	agent_name = gets.chomp
 end
-
+p agent_name
+def next_vowel
 #swap the first and last names
 reverse_agent_name = agent_name.split(" ").reverse!
 p reverse_agent_name
 #split the agent's name into letters 
 letters = reverse_agent_name.map!{|name| name = name.chars}
 p letters
-# #array for vowels
-# vowels = "aeiou".chars
-# #array for consonants
-# 	if [letters] == vowels
-# 		puts vowels.next
-# 	else
-# 		puts 
-# 	end
-#end
+# 	#array for vowels
+ 	vowels = "aeiou".chars
+# 	#array for consonants
+# 	next_consonant = "bcdfghjklmnpqrstvwxyz".chars
+ 	if letters.include?(vowels)
+ 		vowels.map!{|vowel|vowel = vowel.shift(1)}
+ 	else
+	 	letters.each{|letter| letter = letter.shift(2)}	
+ 	end
 
-# p next_letter
-
-# consonants = "bcdfghjklmnpqrstvwxyz".chars
-# #method for next vowel
-# def next_vowel 
-# 	if letters == vowels 
-# 	puts vowels.next
-# end	
-# agent_name[/(?<vowel>[aeiou](?<non-vowel>[^aeiou])/, "vowel"]
-
-#check letters of agent's name for vowels and consonants
-# if letters == vowels
-# 	puts next_vowel
-# else
-# 	puts letters.map!{
-# 	|letter| letter.next
-# 	}
-# end		
-# if letters.include? vowels
-# 	puts next_vowel
-# elsif letters.include?(consonants)
-# 	puts next_consonant
-# else
-# 	puts "error"
-# end
-
-
-			
-	
-#next vowel method
-def next_vowel
-	vowels = "aeiou".chars
-	vowels.next
-end	
+end
+p next_vowel
 
 	
-# if agent_name.include?(vowels)
-# 	put next_vowel
-# end
+
+
 
 
 
