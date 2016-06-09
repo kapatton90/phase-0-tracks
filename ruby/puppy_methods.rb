0493279c6d8887b6180065599ec044be8064b9ce
kapatton19 <kapatton19@gmail.com>
2016-06-08 21:24:10 -0500

    Create Kitten class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,60 @@
+#Dylan Coalwell & Kimberly Patton
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end		
+
+	def fetch(toy)
+	  puts "I brought back the #{toy}!"
+	  toy
+	end
+
+	def speak(x)
+	    x.times { puts "Woof!" }
+	end
+
+	def roll_over
+	    puts "​*Rolls over*​"
+	end
+
+	def dog_years(human_years)
+	     human_years * 7
+	end
+
+	def play_dead
+	    puts "​*falls on floor and sticks tongue out of mouth*​"
+	end
 
 end
 
+max = Puppy.new
+max.fetch("ball")
+max.speak(6)
+max.roll_over
+p max.dog_years(23)
+max.play_dead
+
+class Kitten
+
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
+	
+	def yarn
+		puts "*rolls ball of yarn around*"
+	end
+	
+	def cat_lives(human_lives)
+		human_lives * 9		
+	end
+	
+	def shows_affection
+		puts "*purr*"
+	end
+end
+
+kittens_array = []
+
+50.times {kittens_array << Kitten.new }			
+p kittens_array
