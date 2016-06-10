8f648e9daeefa2cceea312c438b551b2fb3629da
kapatton19 <kapatton19@gmail.com>
2016-06-10 12:48:37 -0500

    Include flight module in bird and plane classes. Add code at the bottom to test.

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -3,10 +3,17 @@ module Flight
 		puts "Taking off and ascending until reaching #{altitude}..."
 	end
 end
-	
+
 class Bird
+	include Flight
 end
 
 class Plane
+	include Flight
 end
 
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(3000)
