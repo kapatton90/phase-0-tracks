de9b48a71eb87d278f59f034a59b6b1d51d3b955
kapatton19 <kapatton19@gmail.com>
2016-06-12 17:13:35 -0500

    Test Santa class for Realease 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -13,17 +13,21 @@ class Santa
 		puts "Ho, ho, ho! Haaaappy holidays"
 	end
 	
+	#Create a method for eating milk and cookies
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
 
+	#Create a method to assign gender
 	def gender=(new_gender)
 		@gender = new_gender
 	end	
 
+	#Create a method to place the reindeer Santa is mad at, at the back of the line
 	def get_mad_at(reindeer)
 	end
 
+	#Creat a method to celebrate a birthday and change the age to a year older
 	def celebrate_birthday
 		@celebrate_birthday = @age += 1
 	end	
@@ -33,4 +37,4 @@ kris = Santa.new("male", "Germanic")
 p kris.celebrate_birthday
 p kris.celebrate_birthday
 kris.gender = "female"
-santas = []
+p Santa
