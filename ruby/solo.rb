1c6e84c2b60d144f8745cbda2ac60b14c97e9457
kapatton19 <kapatton19@gmail.com>
2016-06-12 23:11:03 -0500

    Create method that will add all instances to an array and display their attributes

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,20 +1,26 @@
 #Create a Zombie clas
 class Zombie
+
+	@@zombies_array = Array.new 
 	#Make name readable and writable
-	attr_accessor :hunger_level
-	#Attributes of the class
-	#Boolean for life status
-		@@life_status = "undead"
-			"dead" == false && "alive" == false
-		#String for physical condition
-		@@physical_condition = "decomposing"
-		#Integer indicating how hungry the zombie is
+	attr_accessor :name, :hunger_level
+
+	def self.all_instances
+		@@zombies_array
+	end	
+	
 	#Create an initialize method
 	def initialize(name)
 		puts "#{name} has been recently bitten and is turning into a zombie!!!"
-		#Attributes of each instance of the class
-		
+		#Attributes of the class
+		#Boolean for life status
+		@name = name
+		@life_status = "undead"
+			"dead" == false && "alive" == false
+		#String for physical condition
+		@physical_condition = "decomposing"
 		@hunger_level = hunger_level
+		@@zombies_array << self
 	end
 	
 	#Create a method to moan
@@ -49,7 +55,7 @@ class Zombie
 
 	#Create a method for the Thriller
 	def self.dance
-			puts "*Does Thriller Dance*"	
+			puts "*#{name} Does Thriller Dance*"	
 	end
 
 end
@@ -57,18 +63,20 @@ end
 #Hash for created zombies
 zombies_array = []
 
+
+#Begin zombie experience
+puts "The zombie virus has spread to your town and you being to worry about becoming infected. You decide to head to Costco for supplies so you can baricade yourself in your home if you need to. You step out the front door and..."
 #Get name from user to create their zombie name
 puts "What is your name?"
 name = gets.chomp
 
-name = Zombie.new(name)
+#Create a new instance of Zombie class with name
+Zombie.new(name)
 
-zombies_array << name
-
-p zombies_array
-
+#Use eat brains method for second part of user input
 puts Zombie.eat_brains
 
+#Activate dance method based on user input
 puts "Do you feel like dancing?(yes/no)"
 dance = gets.chomp
 	if dance == "yes"
@@ -76,3 +84,5 @@ dance = gets.chomp
 	else
 		puts "ArRrr, too hungry for dancing, grRrr!!!"
 	end		
+
+p Zombie.all_instances
