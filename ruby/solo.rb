b6098832d71ca16d30309fc95dbd849ecee8b1e9
kapatton19 <kapatton19@gmail.com>
2016-06-12 19:58:25 -0500

    Work on methods for Zombie class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,17 +1,24 @@
 #Create a Zombie clas
 class Zombie
-	
+	#Create an array for new zombies
+		attr_accessor zombies = []
 	#Create an initialize method
 	def initialize
 		#Attributes of the Zombie class
+		@name = name
 		#Boolean for life status
 		@life_status = undead
-			dead == false && alive == false || undead = true
+			dead == false && alive == false
 		#String for physical condition
 		@physical_condition = decomposing
 		#Integer for hungry meter
 		@hunger_range = [0..10]
 	end
+	
+	#use setter method to let user choose a name for their zombie	
+	def name=(new_name)
+		@name = new_name
+	end	
 
 	#Create a method to moan
 	def moan
