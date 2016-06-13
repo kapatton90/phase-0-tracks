e99ade36a0371ad351e7d855127976ee6cf7db60
kapatton19 <kapatton19@gmail.com>
2016-06-12 23:33:31 -0500

    Include questions for user to moan or shuffle.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,10 +1,12 @@
 #Create a Zombie clas
 class Zombie
-
+	#Array to store Zombie class instances
 	@@zombies_array = Array.new 
 	#Make name readable and writable
-	attr_accessor :name, :hunger_level
+	attr_reader :hunger_level
+	attr_accessor :name
 
+	#Method to keep all instance of Zombie class in array
 	def self.all_instances
 		@@zombies_array
 	end	
@@ -13,27 +15,20 @@ class Zombie
 	def initialize(name)
 		puts "#{name} has been recently bitten and is turning into a zombie!!!"
 		#Attributes of the class
-		#Boolean for life status
 		@name = name
 		@life_status = "undead"
-			"dead" == false && "alive" == false
-		#String for physical condition
 		@physical_condition = "decomposing"
 		@hunger_level = hunger_level
+		#Array for all instances to be put in zombies_array
 		@@zombies_array << self
-	end
-	
-	#Create a method to moan
-	def moan(x)
-		x.times{puts "Graagh brains!"}
 	end
 	
 	#Create a method to eat brains
 	def self.eat_brains
 		#Ask how hungry they are to see how much they will want to eat brains
 		puts "How hungry are you on a scale of one to ten?(Ten being the most hungry)"
-		@hunger_level = gets.chomp.to_i
-
+		hunger_level = gets.chomp.to_i
+		@hunger_level = hunger_level
 		#Case statement to decide whether or not to eat brains
 		case @hunger_level
 			
@@ -47,34 +42,35 @@ class Zombie
 				puts "Not hungry, but will eat brains anyway."			
 		end
 	end
-
-	#Create a method for the Zombie walk
-	def shuffle
-		puts "*shuffles around aimlessly*"
-	end
-
+	
 	#Create a method for the Thriller
 	def self.dance
 			puts "*#{name} Does Thriller Dance*"	
 	end
 
+	#Create a method for the Zombie walk
+	def self.shuffle
+		puts "*shuffles around aimlessly*"
+	end
+
+	#Create a method to moan
+	def self.moan(x)
+		x.times{puts "GrrRrr aagh brains!"}
+	end
 end
 
-#Hash for created zombies
-zombies_array = []
-
-
 #Begin zombie experience
-puts "The zombie virus has spread to your town and you being to worry about becoming infected. You decide to head to Costco for supplies so you can baricade yourself in your home if you need to. You step out the front door and..."
 #Get name from user to create their zombie name
 puts "What is your name?"
 name = gets.chomp
 
+puts "The zombie virus has spread to your town and you being to worry about becoming infected. You decide to head to Costco for supplies so you can baricade yourself in your home if you need to. You step out the front door and..."
+
 #Create a new instance of Zombie class with name
 Zombie.new(name)
 
-#Use eat brains method for second part of user input
-puts Zombie.eat_brains
+#Eat brains method for hunger level
+p Zombie.eat_brains
 
 #Activate dance method based on user input
 puts "Do you feel like dancing?(yes/no)"
@@ -84,5 +80,17 @@ dance = gets.chomp
 	else
 		puts "ArRrr, too hungry for dancing, grRrr!!!"
 	end		
+
+puts "Now that you are a zombie, there isn't much to do, other than eat brains of course. Would you like to shuffle or moan?"
+	decision = gets.chomp
+	if decision == "shuffle"
+		puts Zombie.shuffle
+	elsif decision == "moan"
+		puts "How many times would you like to moan?"
+		moan_times = gets.chomp.to_i
+		puts Zombie.moan(moan_times)
+	else
+		puts "No time for nonsense! Need more brains!!!! grRrr"
+	end				
 
 p Zombie.all_instances
