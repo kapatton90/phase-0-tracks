3b2c175ac18d695899a0a9506c204f97cfb751e4
kapatton90 <kapatton19@gmail.com>
2016-06-19 18:32:10 -0500

    Create an arry of horse colors and an array of horse names.

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var horseColors = ["blue", "green", "orange", "purple"];
+
+var horseNames = ["Ed", "Wildfire", "Black Beauty", "Hidalgo"];
+
+horseColors.push("red");
+
+horseNames.push("Thunder");
