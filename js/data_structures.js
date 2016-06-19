46f796be2991311b9052b8a675ac932ee38a5562
kapatton90 <kapatton19@gmail.com>
2016-06-19 18:35:51 -0500

    Add a new name to the names array and a new color to the colors array.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,11 @@
+//Create an array of horse colors
 var horseColors = ["blue", "green", "orange", "purple"];
-
+//Create an array of horse names
 var horseNames = ["Ed", "Wildfire", "Black Beauty", "Hidalgo"];
-
+//Add a new color to the colors array 
 horseColors.push("red");
-
+//Add a new name to the names array
 horseNames.push("Thunder");
+
+console.log(horseNames);
+
