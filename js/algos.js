23aac3cc7ff97b70867743f77c72ed2ca2006ab9
kapatton90 <kapatton19@gmail.com>
2016-06-21 05:53:53 -0500

    Finish Release 0. Add comments, and make more DRY.

--- js/algos.js
+++ js/algos.js
@@ -2,14 +2,24 @@
 //returns the longest word or phrase in the array.
 //Create an array of words and phrase
 var phraseArray = ["My name is Kimberly", "hello", "Kimberly", "turtle", "nonsense"];
-//Iterate throught the array
-for(var i = 0; i < phraseArray[i].length; i++){
-	var phraseLength = [];
-	console.log("The longest word in this array is" );
-}
-
-
-// //Define a funtion
-// var longestWord = function(){
-// 	return something;
-// }
+//Create a funtion
+var longestWord = function(array){
+	//Create a variable for the longest element of the array
+	var longestElement = "";
+	//Set it equal to zero as a start
+	var i = 0;
+	//Iterate through the array
+	for(var i = 0; i < array.length; i++){
+		//IF statement to replace longest Element with each
+		//by comparing each element's length
+		if(array[i].length > longestElement.length){
+			 //Variable changes each time an element is longer
+			 //than the last one
+			 longestElement = array[i];
+		};		
+	};
+	//Return to make sure a value is given back 
+	return longestElement;
+};
+//Test longestWord function
+console.log(longestWord(phraseArray));
